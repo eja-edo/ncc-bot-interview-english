5e79a22969fea182654a8737f31a0231557d6e3b
Nguyễn Lê Đức Hiếu <hieu.nguyenleduc@ncc.asia>
2025-12-19 14:53:04 +0700

    config port env

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { getQueueToken } from "@nestjs/bull";
+import { ConfigService } from "@nestjs/config";
 import { AppModule } from "@/app.module";
 import { setupBullBoard } from "@/bull-board/bull-board.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT')
+
   const ttsQueue = app.get(getQueueToken("tts"));
   const serverAdapter = setupBullBoard([ttsQueue]);
 
   app.use("/admin/queues", serverAdapter.getRouter());
 
-  await app.listen(3000);
-  console.log(`Application is running on: http://localhost:3000`);
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
   console.log(
-    `Bull Board UI is available at: http://localhost:3000/admin/queues`
+    `Bull Board UI is available at: http://localhost:${port}/admin/queues`
   );
 }
 bootstrap();
